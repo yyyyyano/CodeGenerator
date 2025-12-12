463844e9975f2cd7313e6e3f30d0c1d9fded1ec3
yyyyyano <145003765+yyyyyano@users.noreply.github.com>
2025-12-12 21:47:04 +0300

    Update run.sh

--- CodeGenerator/run.sh
+++ CodeGenerator/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source venv/bin/activate
 
-# Убиваем старый процесс, если висит
 if lsof -i :5000 > /dev/null 2>&1; then
     echo "Порт 5000 занят — убиваем старый процесс..."
     sudo kill -9 $(sudo lsof -t -i:5000) 2>/dev/null || true
